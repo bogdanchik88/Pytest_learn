3018f171ef506a1eb6c3b6d4e2b0e9a23887cb34
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-03-26 22:47:11 +0300

    finishing other routes

--- src/main.py
+++ src/main.py
@@ -4,7 +4,42 @@ app = FastAPI(
     title='Super Puper App'
 )
 
-@app.get('/')
+@app.get('/api')
 def health():
     return {'message': 'hello fastapi'}
 
+@app.get('/api/append')
+def append(a: int | float, b: int | float):
+    return {
+        'operation': 'append',
+        'first_number': a,
+        'second_number': b,
+        'result': a + b
+    }
+
+@app.get('/api/substract')
+def append(a: int | float, b: int | float):
+    return {
+        'operation': 'append',
+        'first_number': a,
+        'second_number': b,
+        'result': a - b
+    }
+
+@app.get('/api/multiply')
+def append(a: int | float, b: int | float):
+    return {
+        'operation': 'append',
+        'first_number': a,
+        'second_number': b,
+        'result': a * b
+    }
+
+@app.get('/api/divide')
+def append(a: int | float, b: int | float, div_integer: bool = False):
+    return {
+        'operation': 'append',
+        'first_number': a,
+        'second_number': b,
+        'result': a + b
+    }
