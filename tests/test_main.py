c9d1627739b38199c0984b8a0ac98976fbdad24a
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-03-27 09:46:28 +0300

    added negative cases to append endpoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,18 +4,20 @@ def test_func():
     assert 2 == 2
 
 @pytest.mark.parametrize(
-        'a, b, res',
+        'a, b, res, status_code',
         [
-            (1, 2, 3),
-            (4, 5, 9),
-            (0, 0, 0),
+            (1, 2, 3, 200),
+            (4, 5, 9, 200),
+            (0, 0, 0, 200),
+            (0, 'hello', None, 422)
         ]
 )
-def test_append(a, b, res, client):
-    responce = client.get(f'/api/append?a={a}&b={b}')
+def test_append(a, b, res, status_code, client):
+    responce = client.get('/api/append', params={'a': a, 'b': b})
 
-    assert responce.status_code == 200
-    assert responce.json()['result'] == res
+    assert responce.status_code == status_code
+    if res:
+        assert responce.json()['result'] == res
 
 def test_substract(client):
     responce = client.get('/api/substract?a=8&b=3')
