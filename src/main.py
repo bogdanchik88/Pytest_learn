c116e4b2b17932a4f3d38658528d3b04565f5c8c
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-03-27 11:25:26 +0300

    Updating dividing endpoint for integer dividing

--- src/main.py
+++ src/main.py
@@ -37,9 +37,12 @@ def append(a: int | float, b: int | float):
 
 @app.get('/api/divide')
 def append(a: int | float, b: int | float, div_integer: bool = False):
+
+    result = a / b if div_integer else a // b
+
     return {
         'operation': 'append',
         'first_number': a,
         'second_number': b,
-        'result': a / b
+        'result': result
     }
