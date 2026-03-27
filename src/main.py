9c93b55e4be80d7245373c25331e010cb2a911b7
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-03-28 00:22:55 +0300

    api methods fix

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI(
     title='Super Puper App'
@@ -18,30 +18,33 @@ def append(a: int | float, b: int | float):
     }
 
 @app.get('/api/substract')
-def append(a: int | float, b: int | float):
+def substract(a: int | float, b: int | float):
     return {
-        'operation': 'append',
+        'operation': 'substract',
         'first_number': a,
         'second_number': b,
         'result': a - b
     }
 
 @app.get('/api/multiply')
-def append(a: int | float, b: int | float):
+def multiply(a: int | float, b: int | float):
     return {
-        'operation': 'append',
+        'operation': 'multiply',
         'first_number': a,
         'second_number': b,
         'result': a * b
     }
 
 @app.get('/api/divide')
-def append(a: int | float, b: int | float, div_integer: bool = False):
+def divide(a: int | float, b: int | float, div_integer: bool = False):
 
-    result = a // b if div_integer else a / b
+    if b == 0:
+        raise HTTPException(status_code=400, detail='Cannot divide by zero!')
+
+    result = a // b if div_integer else round(a / b, 2)
 
     return {
-        'operation': 'append',
+        'operation': 'divide',
         'first_number': a,
         'second_number': b,
         'result': result
