f33ab5dc4b409e25df37f2170e02f5f1bcfd7570
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-03-27 10:07:28 +0300

    finalizing substraction and appending tests

--- tests/test_substract.py
+++ tests/test_substract.py
@@ -4,10 +4,20 @@ import pytest
         'a, b, res, status_code',
         [
             (5, 2, 3, 200),
+            (4, 9, -5, 200),
+            (0, 0, 0, 200),
+            (-4, 6, -10, 200),
+            (7, -2, 9, 200),
+            (-8, -9, 1, 200),
+            ('1', 5, -4, 200),
+            (3, '-8', 11, 200),
+            ('-4', '2', -6, 200),
+            ('darov', [1, 2, 3], None, 422),
         ]
 )
 def test_substract(client, a, b, res, status_code):
     responce = client.get('/api/substract', params= {'a': a, 'b': b})
 
     assert responce.status_code == status_code
-    assert responce.json()['result'] == res
+    if res:
+        assert responce.json()['result'] == res
